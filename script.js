f5d98c4b100d41c560211e1148ab6f9b18ac8702
Darine <tagdarinefatma@gmail.com>
2024-01-13 15:05:13 +0100

    add js variables

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
+function cleanInputString(str) {
+  const strArray = str.split("");
+  const cleanStrArray = [];
+  for (let i = 0; i < strArray.length; i++) {}
+}
