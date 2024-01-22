93f8064320f87cd9855dc25c96d73b3032ab066d
Darine <tagdarinefatma@gmail.com>
2024-01-22 14:30:29 +0100

    update function

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ function isInvalidInput(str) {
 function addEntry() {
   const targetId = "#" + entryDropdown.value;
   const targetInputContainer = document.querySelector(
-    targetId + " .input-container"
+    `${targetId} +  .input-container`
   );
 }
