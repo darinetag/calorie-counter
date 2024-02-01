af10fdc1535f7ab1a6d6c6b1ae88dd493038baa1
Darine <tagdarinefatma@gmail.com>
2024-02-01 11:57:53 +0100

    update function

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetInputContainer = document.querySelector(
-    `#${entryDropdown.value} .input-container`
-  );
-  const entryNumber = targetInputContainer.querySelectorAll();
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = ``;
 }
