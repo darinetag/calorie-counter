60f36ba19bcf513a140061caebabf110aeef1b17
Darine <tagdarinefatma@gmail.com>
2024-02-02 15:42:11 +0100

    add label element

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-  const HTMLString = ``;
+  const targetInputContainer = document.querySelector(
+    `#${entryDropdown.value} .input-container`
+  );
+  const entryNumber =
+    targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `<label>Entry ${entryNumber} Name</label>`;
 }
