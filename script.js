059abd8424ec6d383c8519e2f9155a54e38e261b
Darine <tagdarinefatma@gmail.com>
2024-02-03 11:55:40 +0100

    add a for loop

--- script.js
+++ script.js
@@ -26,6 +26,11 @@ function addEntry() {
   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
   <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"></input>`;
-  targetInputContainer.insertAdjacentHTML();
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+  for (let i = 0; i < list.length; i++) {}
 }
 addEntryButton.addEventListener("click", addEntry);
