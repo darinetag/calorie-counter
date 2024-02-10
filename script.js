3d41d5efc808e5c82ca7cda1ed6c51c609aeebb8
Darine <tagdarinefatma@gmail.com>
2024-02-10 17:32:15 +0100

    update if statement

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ function getCaloriesFromInputs(list) {
     const isInvalidInput = isInvalidInput(currVal);
     if (invalidInputMatch) {
       alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
     }
   }
 }
