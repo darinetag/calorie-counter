8086d87acb47a67cc8618cebf06f0390cb5cdeb6
Darine <tagdarinefatma@gmail.com>
2024-02-17 22:39:21 +0100

    check the truthiness of the global error flag

--- script.js
+++ script.js
@@ -58,6 +58,10 @@ function calculateCalories(e) {
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
   const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+  if (isError) {
+    return;
+  }
 }
 function getCaloriesFromInputs(list) {
   let calories = 0;
