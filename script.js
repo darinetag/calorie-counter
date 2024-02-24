6aadc40ee0c3a67d21afb76dde59d5aacb02f158
Darine <tagdarinefatma@gmail.com>
2024-02-24 22:45:50 +0100

    add span

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function calculateCalories(e) {
   const remainingCalories =
     budgetCalories - consumedCalories + exerciseCalories;
   const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
-  output.innerHTML = ``;
+  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${remainingCalories} Calorie ${surplusOrDeficit}</span>`;
 }
 function getCaloriesFromInputs(list) {
   let calories = 0;
