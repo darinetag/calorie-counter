32867489ec11c92df86de7ed2d71f0c095ef7357
Darine <tagdarinefatma@gmail.com>
2024-02-29 21:40:58 +0100

    restore the hide class

--- script.js
+++ script.js
@@ -107,6 +107,9 @@ function clearForm() {
   for (const container of inputContainers) {
     container.innerHTML = "";
   }
+  budgetNumberInput.value = "";
+  output.innerText = "";
+  output.classList.add("hide");
 }
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
